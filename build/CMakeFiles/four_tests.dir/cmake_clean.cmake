0a29bb5f4590e9f4ba359e32b8ce206b1eacb8b6
Стальной Иван Евгеньевич <vanya.stalnoy@gmail.com>
2025-10-06 14:20:46 +0300

    решение

---
+++ build/CMakeFiles/four_tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/four_tests.dir/Four.cpp.o"
+  "CMakeFiles/four_tests.dir/Four.cpp.o.d"
+  "CMakeFiles/four_tests.dir/test_four.cpp.o"
+  "CMakeFiles/four_tests.dir/test_four.cpp.o.d"
+  "four_tests"
+  "four_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/four_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
